a9d3c254985f64a619a1439c75353f7916cf24ac
antonifarinango <farinangoanthony1@gmail.com>
2024-11-06 23:07:40 -0500

    enlaces y estilos

--- src/components/SobreNosotros.jsx
+++ src/components/SobreNosotros.jsx
@@ -14,8 +14,38 @@ function SobreNosotros(){
                         <img className="img-sn" src={img} alt="" />
                     </div>
                     <div className="textoSobreNosotros  text-light ">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia sed fuga repellat nobis. Similique minima fugiat ea, aperiam perferendis doloribus tenetur quis non laboriosam architecto 
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, vel qui dolorum similique quam consequatur. Ipsum fugit magni sit illum itaque excepturi accusantium vitae vel deserunt, provident ex. Eaque, vitae?
+                        <p>
+                        Suway (Corazón Joven) es una agrupación musical dedicada a fusionar la música tradicional ecuatoriana con el estilo andino, manteniendo vivas las tradiciones y costumbres de nuestra cultura, pero con una mirada actual y fresca. Nos esforzamos por conectar generaciones a través de sonidos que reflejan nuestras raíces y que hablan de quiénes somos.
+                        </p>
+
+                        <h4>Nuestra Historia</h4>
+                        <p>
+                            Fundada el 26 de octubre de 2012, Suway cuenta con ocho talentosos integrantes que han compartido escenario con grandes exponentes de la música ecuatoriana como Jayac, Puro Corazón, Los Amigos Millonarios, y Los Quitus, entre otros. Con años de trayectoria, seguimos llevando nuestra propuesta musical a nuevos escenarios y públicos.
+                        </p>
+                        
+                        <h4>Experiencias Destacadas</h4>
+                        <p>
+                        Participación en el programa de las Flores y las Frutas en Ambato
+                        Invitación a las festividades de Ibarra
+                        Presentación de un tema inédito en el evento Maicito de Oro en Zámbiza
+                        Misión
+                        Nuestra misión es conectar con la gente a través de nuestra música, ofreciendo una fusión entre la tradición ecuatoriana y el folklore andino que resuene en el corazón de quienes nos escuchan.
+                        </p>
+                        
+
+                        <h4>Visión</h4>
+                        <p>
+                            Aspiramos a que, a través de la música, podamos llegar a distintos rincones de Ecuador y el mundo, dejando un legado de esfuerzo y constancia.
+                        </p>
+                        
+
+                        <h4>Síguenos</h4> 
+                        <p>
+                            ¡Conoce más sobre nosotros en nuestras redes sociales! Encuéntranos en Facebook como <a target="_blank" href="https://www.facebook.com/suwaycorazonjoven" className="link-sobre-nosotros">Suway</a> y en TikTok como <a target="_blank" href="https://www.tiktok.com/@suway.corazn.jove?_t=8rBXwf6oRXT&_r=1" className="link-sobre-nosotros">Suway Corazón Joven</a>.
+                        </p>
+                        
+                        
+                    
                     </div>
                     </div>
                     
